b5967304159ea146cc2f80f103c3d5ebcc43cfa5
Duvis07 <96325513+Duvis07@users.noreply.github.com>
2022-05-20 00:13:53 -0500

    REFACTOR: Objetos de valor
    se hacen  cambios de los objetos de valor de cada uno de los agregados

--- dominio-ddd-heladeria/domain/src/main/java/co/com/sofkau/entrenamiento/curso/pedido/events/PedidoAsignado.java
+++ dominio-ddd-heladeria/domain/src/main/java/co/com/sofkau/entrenamiento/curso/pedido/events/PedidoAsignado.java
@@ -1,19 +1,35 @@
 package co.com.sofkau.entrenamiento.curso.pedido.events;
 
 import co.com.sofka.domain.generic.DomainEvent;
+import co.com.sofkau.entrenamiento.curso.envio.values.Nombre;
 import co.com.sofkau.entrenamiento.curso.pedido.identities.IdPedido;
+import co.com.sofkau.entrenamiento.curso.pedido.values.Descripcion;
 
 public class PedidoAsignado extends DomainEvent {
 
     private final IdPedido idPedido;
 
-    public PedidoAsignado(IdPedido idPedido ){
+    private final Nombre nombre;
+
+    private final Descripcion descripcion;
+
+    public PedidoAsignado(IdPedido idPedido, Nombre nombre, Descripcion descripcion){
         super("co.com.sofkau.entrenamiento.curso.PedidoAsignado ");
+
         this.idPedido = idPedido;
+        this.nombre = nombre;
+        this.descripcion = descripcion;
     }
 
-
-    public IdPedido getIdPedido() {
+    public IdPedido IdPedido() {
         return idPedido;
+    }
+
+    public Nombre Nombre() {
+        return nombre;
+    }
+
+    public Descripcion Descripcion() {
+        return descripcion;
     }
 }
--- dominio-ddd-heladeria/domain/src/main/java/co/com/sofkau/entrenamiento/curso/pedido/events/ProductoReemplazado.java
+++ dominio-ddd-heladeria/domain/src/main/java/co/com/sofkau/entrenamiento/curso/pedido/events/ProductoReemplazado.java
@@ -1,22 +1,42 @@
 package co.com.sofkau.entrenamiento.curso.pedido.events;
 
 import co.com.sofka.domain.generic.DomainEvent;
-import co.com.sofkau.entrenamiento.curso.pedido.identities.IdPedido;
+import co.com.sofkau.entrenamiento.curso.envio.values.Nombre;
 import co.com.sofkau.entrenamiento.curso.pedido.identities.IdProducto;
+import co.com.sofkau.entrenamiento.curso.pedido.values.Cantidad;
+import co.com.sofkau.entrenamiento.curso.pedido.values.Sabor;
 
 public class ProductoReemplazado extends DomainEvent {
 
     private final IdProducto idProducto;
 
-    public ProductoReemplazado(IdProducto idProducto) {
-        super("co.com.sofkau.entrenamiento.curso.ProductoReemplazado");
+    private final Nombre nombre;
 
+    private final Sabor sabor;
+
+    private final Cantidad cantidad;
+
+    public ProductoReemplazado(IdProducto idProducto, Nombre nombre, Sabor sabor, Cantidad cantidad) {
+        super("co.com.sofkau.entrenamiento.curso.ProductoReemplazado");
         this.idProducto = idProducto;
+        this.nombre = nombre;
+        this.sabor = sabor;
+        this.cantidad = cantidad;
     }
 
-
-    public IdProducto idProducto() {
+    public IdProducto IdProducto() {
         return idProducto;
     }
 
+    public Nombre Nombre() {
+        return nombre;
+    }
+
+    public Sabor Sabor() {
+        return sabor;
+    }
+
+    public Cantidad Cantidad() {
+        return cantidad;
+    }
 }
